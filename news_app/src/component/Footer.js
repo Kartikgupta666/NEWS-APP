ba6a5b0f32e9fb3fe3711e39222e2befea369dff
kartikgupta666 <kartikgangil@gmail.com>
2024-02-24 10:31:59 +0530

    about us page updated

--- news_app/src/component/Footer.js
+++ news_app/src/component/Footer.js
@@ -7,7 +7,7 @@ export class Footer extends Component {
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="col-md-4 d-flex align-items-center">
             <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-              <i className="" style={{ 'font-size': '26px' }}>N.E.W.S</i>
+              <i className="" style={{ 'fontSize': '26px' }}>N.E.W.S</i>
             </a>
 
           </div>
@@ -15,10 +15,10 @@ export class Footer extends Component {
 
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
 
-            <li className="ms-3"><a className="text-muted" target='_blank' href="https://github.com/Kartikgupta666/NEWS-APP.git"><i className='fa fa-github' style={{ 'font-size': '26px' }}></i></a></li>
-            <li className="ms-3"><a className="text-muted" target='_blank' href="https://www.instagram.com/its_kartik_6666"> <i className='fa fa-instagram' style={{ 'font-size': '26px' }}></i> </a></li>
-            <li className="ms-3"><a className="text-muted" target='_blank' href="https://www.linkedin.com/in/kartik-gangil"><i className="fa fa-linkedin" style={{ 'font-size': '26px' }}></i></a></li>
-            <li className="ms-3"><a className="text-muted" target='_blank' href="https://www.youtube.com/channel/UCIKzs6MlFXQvgQbAAaH0GyA"><i className="fa fa-youtube-play" style={{ 'font-size': '26px' }}></i></a></li>
+            <li className="ms-3"><a className="text-muted"  rel="noreferrer" target='_blank' href="https://github.com/Kartikgupta666/NEWS-APP.git"><i className='fa fa-github' style={{ 'fontSize': '26px' }}></i></a></li>
+            <li className="ms-3"><a className="text-muted"  rel="noreferrer" target='_blank' href="https://www.instagram.com/its_kartik_6666"> <i className='fa fa-instagram' style={{ 'fontSize': '26px' }}></i> </a></li>
+            <li className="ms-3"><a className="text-muted"  rel="noreferrer" target='_blank' href="https://www.linkedin.com/in/kartik-gangil"><i className="fa fa-linkedin" style={{ 'fontSize': '26px' }}></i></a></li>
+            <li className="ms-3"><a className="text-muted"  rel="noreferrer" target='_blank' href="https://www.youtube.com/channel/UCIKzs6MlFXQvgQbAAaH0GyA"><i className="fa fa-youtube-play" style={{ 'fontSize': '26px' }}></i></a></li>
 
           </ul>
         </footer >
--- news_app/src/component/NavBar.js
+++ news_app/src/component/NavBar.js
@@ -6,7 +6,7 @@ export class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/home">N.E.W.S</Link>
+                        <Link className="navbar-brand" to="/">N.E.W.S</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
